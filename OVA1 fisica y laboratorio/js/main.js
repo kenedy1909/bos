29735d451760ed2c8fee86bc3936fd31a26e7eb5
miguel8888888 <miguelsgap@gmail.com>
2021-07-22 09:27:17 -0500

    actualizacion
    actividades y poecentajes

--- OVA1 fisica y laboratorio/js/main.js
+++ OVA1 fisica y laboratorio/js/main.js
@@ -100,9 +100,9 @@ function actualizarprogress(){
     var unidad = $(".container-fluid").data("unidad");
     if(unidad == 1){
         naveg = 0;
-    }else if(unidad == 2){
+    }/*else if(unidad == 2){
         naveg = 10;
-    }
+    }*/
     done = $(".done").length;
     if(unidad == 2){
         done--;
@@ -110,7 +110,7 @@ function actualizarprogress(){
     /*alert(naveg);
     alert(done);
     alert(active);*/
-    total_porcentaje = parseInt(((done+active+naveg)*100)/23);
+    total_porcentaje = parseInt(((done+active+naveg)*100)/20);
     $(".number").html(total_porcentaje+'%');
     var pixel = parseInt(((total_porcentaje*157)/100)+200);
     /*alert(pixel);
