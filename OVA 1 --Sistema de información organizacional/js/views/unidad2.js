dd393ed97928630338b400d3fc811ae820ea3779
kenedy1909 <jhonatangarcia.r05@gmail.com>
2021-07-22 13:50:51 -0500

    update

--- OVA 1 --Sistema de información organizacional/js/views/unidad2.js
+++ OVA 1 --Sistema de información organizacional/js/views/unidad2.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+    $( ".zoomContainer" ).remove();
     $('#smartwizard').smartWizard({
         loader:"show",
         theme:'arrows',
@@ -13,7 +14,7 @@ $( document ).ready(function() {
             easing:'' // Transition animation easing. Not supported without a jQuery easing plugin
         }
     });
-
+    $('#zoom_img1').elevateZoom({zoomWindowPosition: 10});
     $('#next').on('click', function () {
         $('#smartwizard').smartWizard("next");
         slide();
@@ -471,7 +472,9 @@ function cajas(num){
 function informacion(num) {
     if (num == 1) {
         $("#informacion_num").html('<h1 class="m-auto"><b>1</b></h1>');
-        $("#informacion_img").html('<img src="assets/img/img_ova/Entrada.png" class="img-100 m-auto">');
+        $("#informacion_img").html('<img id="zoom_img1" src="assets/img/img_ova/Entrada.png" data-zoom-image="assets/img/img_ova/Entrada.png" class="img-100 m-auto ">');
+        $(".zoomContainer" ).remove();
+        $('#zoom_img1').elevateZoom({zoomWindowPosition: 10});
     }else if (num == 2) {
         $("#informacion_num").html('<h1 class="m-auto"><b>2</b></h1>');
         $("#informacion_img").html('<img src="assets/img/img_ova/Almacenamiento.png" class="img-100 m-auto">');
