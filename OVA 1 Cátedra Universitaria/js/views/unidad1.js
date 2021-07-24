0c521db044936854fea348a1e89f10e420e8b2c7
miguel8888888 <miguelsgap@gmail.com>
2021-07-24 01:51:00 -0500

    actualizacion
    empezando funcionalidad

--- OVA 1 Cátedra Universitaria/js/views/unidad1.js
+++ OVA 1 Cátedra Universitaria/js/views/unidad1.js
@@ -33,9 +33,9 @@ $( document ).ready(function() {
     diff = "";
     newTop = "";
     scrollOffset = "";
-    knob = document.querySelector('.custom-scrollbar__knob4');
-    bar = document.querySelector('.custom-scrollbar__bar4');
-    container = document.querySelector('.custom-scrollbar__inner4');
+    knob = "";
+    bar = "";
+    container = "";
     scroll();
 
     slide_link(tema);
@@ -115,60 +115,13 @@ function controlSlides(num){
     }
 }
 
-function objetivos(num){
-    switch (parseInt(num)) {
-        
-        case 1:
-            $('.visible1').css({'visibility': 'visible'});
-            break;
-        case 2:
-            $('.visible2').css({'visibility': 'visible'});
-            break;
-        case 3:
-            $('.visible3').css({'visibility': 'visible'});
-            break;
-        default:
-            break;
-    }
-}
 
-function infoactivi(num) {
-    switch (parseInt(num)) {
-        
-        case 1:
-            $('.info1').css({'visibility': 'visible'});
-            
-            break;
-        case 2:
-            $('.info2').css({'visibility': 'visible'});
-            break;
-        default:
-            break;
-    }
+function scroll_uno() {
+    knob = document.querySelector('.custom-scrollbar__knob_uno');
+    bar = document.querySelector('.custom-scrollbar__bar_uno');
+    container = document.querySelector('.custom-scrollbar__inner_uno');
+    scroll();
 }
-function infoactivi2(num) {
-    switch (parseInt(num)) {
-        
-        case 1:
-            $('.info1_1').css({'visibility': 'visible'});
-            dragging = "";
-            diff = "";
-            newTop = "";
-            scrollOffset = "";
-            knob = document.querySelector('.custom-scrollbar__knob5');
-            bar = document.querySelector('.custom-scrollbar__bar5');
-            container = document.querySelector('.custom-scrollbar__inner5');
-            scroll();
-            break;
-        case 2:
-            $('.info2_1').css({'visibility': 'visible'});
-            break;
-        default:
-            break;
-    }
-}
-
-
 
 /*var knob = document.querySelector('.custom-scrollbar__knob2');
 var bar = document.querySelector('.custom-scrollbar__bar2');
@@ -278,3 +231,125 @@ function Input(num) {
   });
 }
 
+function funcionalidad_1(num) {
+    switch(parseInt(num)){
+        case 1:
+            /*$("#funcionalidad-1").html();*/
+            scroll_uno();
+            break;
+        case 2:
+            /*$("#funcionalidad-1").html();*/
+            $(".i1").html('')    
+            var elementoPadre1 = document.querySelector(".inputDiv.i1");
+            var elementoPadre2 = document.querySelector(".inputDiv.i2");
+            var inputsRy = [];  
+            var i = new Input(12);
+            i.crear(elementoPadre1);
+            inputsRy.push(i);
+
+            var i2 = new Input(12);
+            i2.att.value = 70;
+            i2.att.min = 20;
+            i2.att.max = 120;
+            i2.crear(elementoPadre2);
+            inputsRy.push(i2);
+
+            for (var n = 0; n < inputsRy.length; n++) {
+              (function(n) {
+                inputsRy[n].input.addEventListener("input", function() {
+                  inputsRy[n].actualizar();
+                }, false)
+              }(n));
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+function scroll_horizontal(num){
+    switch (parseInt(num)) {
+        case 0:
+            $('#funcionalidad_1_2').html('');
+            break;
+        case 1:
+            $('#funcionalidad_1_2').html(`
+                                <div class="bg-blanco radius-2 p-3 d-flex">
+                                    <div class="img-70 my-auto px-3">
+                                        <p class="justificado p_black">En 1965, surge la huelga en la Universidad de Antioquia, para destituir al Rector Ignacio Vélez Escobar.</p>
+                                        <br>
+                                        <p class="text-right p_black">Universidad de Antioquia</p>
+                                    </div>
+                                    <div class="img-30">
+                                        <img src="assets/img/img_ova/Imagen9.jpg" class="img-100 m-auto">
+                                    </div>
+                                </div>
+                                `);
+            break;
+        case 2:
+            $('#funcionalidad_1_2').html(`
+                                <div class="bg-blanco radius-2 p-3 d-flex">
+                                    <div class="img-50 my-auto p-5">
+                                        <p class="justificado p_black">Simultáneamente se dan las protestas de las universidades a nivel nacional, por la invasión de los “marines” americanos a Santo Domingo, ambas causas apoyadas por la FUN, creada en 1962.</p>
+                                    </div>
+                                    <div class="img-50 my-auto p-5 border-left-color-c">
+                                        <p class="justificado p_black">El 21 de mayo de 1965 asesinan al estudiante Jorge Enrique Useche, dentro de esas confrontaciones callejeras.</p>
+                                    </div>
+                                </div>
+                                `);
+            break;
+        case 3:
+            $('#funcionalidad_1_2').html(`
+                                <div class="bg-blanco radius-2 p-3 d-flex">
+                                    <div class="img-50 m-auto px-3">
+                                        <p class="justificado p_black">En 1965 se decreta el estado de sitio por el presidente Guillermo León Valencia y prolongado durante los años siguientes por el presidente Carlos Lleras Restrepo./p>
+                                    </div>
+                                    <div class="img-50 my-auto px-3 border-left-color-c">
+                                        <div class="col-md-12 d-flex">
+                                            <div class="img-50 p-3">
+                                                <img src="assets/img/img_ova/Imagen10.jpg" class="img-100 m-auto">
+                                            </div>
+                                            <div class="img-50 p-3">
+                                                <p class="justificado p_black">Guillermo León Valencia.</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12 d-flex">
+                                            <div class="img-50 p-3">
+                                                <img src="assets/img/img_ova/Imagen11.jpg" class="img-100 m-auto">
+                                            </div>
+                                            <div class="img-50 p-3">
+                                                <p class="justificado p_black">Carlos Lleras Restrepo.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                `);
+            break;
+        case 4:
+            $('#funcionalidad_1_2').html(`
+                                <div class="bg-blanco radius-2 p-3 d-flex">
+                                    <div class="img-50 my-auto p-5">
+                                        <p class="justificado p_black">En el año 1966 aparece el Decreto 2128  y otras drásticas medidas como el juzgamiento de los delitos de secuestro, asonada y asociación para delinquir, por la Justicia Penal Militar, aunque fueran cometidos por civiles.</p>
+                                    </div>
+                                    <div class="img-50 my-auto p-5 border-left-color-c">
+                                        <p class="justificado p_black">26 de julio de 1966, Asamblea General de la Universidad de Antioquia, para protestar por la ordenanza 036 de ese año, expedida por la Asamblea Departamental de Antioquia.</p>
+                                    </div>
+                                </div>
+                                `);
+            break;
+        case 5:
+            $('#funcionalidad_1_2').html(`
+                                <div class="bg-blanco radius-2 p-3 d-flex">
+                                    <div class="img-50 my-auto p-5">
+                                        <p class="justificado p_black">El 12 de agosto de 1966, se dá la manifestación de FEDETA y sus filiales por mejores salarios y por cátedra libre, bajo la consigna “unidad obrero estudiantil”.</p>
+                                    </div>
+                                    <div class="img-50 my-auto p-5 border-left-color-c">
+                                        <p class="justificado p_black">El 22 de mayo de 1966, comunicado del consejo estudiantil Universidad de Medellín a la consiliatura, denunciando abusos y malos manejos de las directivas</p>
+                                    </div>
+                                </div>
+                                `);
+            break;
+        default:
+            break;
+    }
+}
