45aba6269ff094bcc59f9737fc915613515a570e
miguel8888888 <miguelsgap@gmail.com>
2021-08-17 21:42:07 -0500

    actualizacion
    pantalla 2 unidad 1

--- OVA 2 inteligencia de negocios/js/views/unidad1.js
+++ OVA 2 inteligencia de negocios/js/views/unidad1.js
@@ -265,42 +265,35 @@ function funcionalidad_1(num) {
 }
 
 function funcionalidad_2(num) {
-    $(".info_funcionalidad_2_1, .info_funcionalidad_2_2, .info_funcionalidad_2_3, .info_funcionalidad_2_4, .info_funcionalidad_2_5, .info_funcionalidad_2_6, .info_funcionalidad_2_7, .info_funcionalidad_2_8").addClass('d-none');
-    $(".btn_funcionalidad_2_1, .btn_funcionalidad_2_2, .btn_funcionalidad_2_3, .btn_funcionalidad_2_4, .btn_funcionalidad_2_5, .btn_funcionalidad_2_6, .btn_funcionalidad_2_7, .btn_funcionalidad_2_8").removeClass('bg-verde-1');
-    $(".btn_funcionalidad_2_1, .btn_funcionalidad_2_2, .btn_funcionalidad_2_3, .btn_funcionalidad_2_4, .btn_funcionalidad_2_5, .btn_funcionalidad_2_6, .btn_funcionalidad_2_7, .btn_funcionalidad_2_8").addClass('bg-color-activi-o');
+    $(".fun_2_content, .fun_2_text_1, .fun_2_text_2, .fun_2_text_3, .fun_2_text_4, .fun_2_text_5, .fun_2_text_6, .fun_2_text_7, .fun_2_text_8, .fun_2_text_9").addClass('d-none');
 
     if (num == 1) {
-        $(".info_funcionalidad_2_1").removeClass('d-none');
-        $(".btn_funcionalidad_2_1").removeClass('bg-color-activi-o');
-        $(".btn_funcionalidad_2_1").addClass('bg-verde-1');
+        $(".fun_2_text_1").removeClass('d-none');
+        $(".fun_2_content").removeClass('d-none');
     }else if (num == 2) {
-        $(".info_funcionalidad_2_2").removeClass('d-none');
-        $(".btn_funcionalidad_2_2").removeClass('bg-color-activi-o');
-        $(".btn_funcionalidad_2_2").addClass('bg-verde-1');
+        $(".fun_2_text_2").removeClass('d-none');
+        $(".fun_2_content").removeClass('d-none');
     }else if (num == 3) {
-        $(".info_funcionalidad_2_3").removeClass('d-none');
-        $(".btn_funcionalidad_2_3").removeClass('bg-color-activi-o');
-        $(".btn_funcionalidad_2_3").addClass('bg-verde-1');
+        $(".fun_2_text_3").removeClass('d-none');
+        $(".fun_2_content").removeClass('d-none');
     }else if (num == 4) {
-        $(".info_funcionalidad_2_4").removeClass('d-none');
-        $(".btn_funcionalidad_2_4").removeClass('bg-color-activi-o');
-        $(".btn_funcionalidad_2_4").addClass('bg-verde-1');
+        $(".fun_2_text_4").removeClass('d-none');
+        $(".fun_2_content").removeClass('d-none');
     }else if (num == 5) {
-        $(".info_funcionalidad_2_5").removeClass('d-none');
-        $(".btn_funcionalidad_2_5").removeClass('bg-color-activi-o');
-        $(".btn_funcionalidad_2_5").addClass('bg-verde-1');
+        $(".fun_2_text_5").removeClass('d-none');
+        $(".fun_2_content").removeClass('d-none');
     }else if (num == 6) {
-        $(".info_funcionalidad_2_6").removeClass('d-none');
-        $(".btn_funcionalidad_2_6").removeClass('bg-color-activi-o');
-        $(".btn_funcionalidad_2_6").addClass('bg-verde-1');
+        $(".fun_2_text_6").removeClass('d-none');
+        $(".fun_2_content").removeClass('d-none');
     }else if (num == 7) {
-        $(".info_funcionalidad_2_7").removeClass('d-none');
-        $(".btn_funcionalidad_2_7").removeClass('bg-color-activi-o');
-        $(".btn_funcionalidad_2_7").addClass('bg-verde-1');
+        $(".fun_2_text_7").removeClass('d-none');
+        $(".fun_2_content").removeClass('d-none');
     }else if (num == 8) {
-        $(".info_funcionalidad_2_8").removeClass('d-none');
-        $(".btn_funcionalidad_2_8").removeClass('bg-color-activi-o');
-        $(".btn_funcionalidad_2_8").addClass('bg-verde-1');
+        $(".fun_2_text_8").removeClass('d-none');
+        $(".fun_2_content").removeClass('d-none');
+    }else if (num == 9) {
+        $(".fun_2_text_9").removeClass('d-none');
+        $(".fun_2_content").removeClass('d-none');
     }
 }
 
