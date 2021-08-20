3829ad2741a6295a50df75c698c1052a2e5a18eb
miguel8888888 <miguelsgap@gmail.com>
2021-08-20 17:44:14 -0500

    inteligencia de negocio 2

--- OVA 2 inteligencia de negocios/js/views/unidad1.js
+++ OVA 2 inteligencia de negocios/js/views/unidad1.js
@@ -13,6 +13,7 @@ $( document ).ready(function() {
             easing:'' // Transition animation easing. Not supported without a jQuery easing plugin
         }
     });
+    $(".linkactividades").attr('href',urlsite+'/course/view.php?id='+courseid);
 
     $('.zoom_img').elevateZoom({zoomWindowPosition: 10});
 
