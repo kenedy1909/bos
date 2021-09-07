099e27f394f87f9ee1bafa7a544ff6040da4110d
Sebastian Aguirre <sebasavmt@gmail.com>
2021-09-07 01:07:40 -0500

    fisica 3

--- OVA 3 LEYES DE MOVIMIENTO_Física_y_Laboratorio (1)/actividades/actividad/actividad_v_o_f_2/script.js
+++ OVA 3 LEYES DE MOVIMIENTO_Física_y_Laboratorio (1)/actividades/actividad/actividad_v_o_f_2/script.js
@@ -11,6 +11,9 @@ function inicioRecurso() {
         {question_txt: "Desde la época de Aristóteles, el movimiento circular de los cuerpos celestiales se consideraba natural.", answer_id: "A", img:""},
         {question_txt: "Aristóteles fundó y desarrolló el campo de la óptica.", answer_id: "B", img:""},
         {question_txt: "Para comprobar su hipótesis de que la gravedad de la Tierra llega a la Luna, Newton comparó la caída de una manzana con la “caída” de la Luna", answer_id: "A", img:""},
+        {question_txt: "Newton razonó que la fuerza sobre cada planeta se dirigiría hacia un punto central fijo: la luna.", answer_id: "B", img:""},
+        {question_txt: "El interés de Newton en la mecánica se reavivó con la aparición de un espectacular cometa en 1680.", answer_id: "A", img:""},
+        {question_txt: "La ley de gravitación universal puede expresarse como una ecuación exacta.", answer_id: "A", img:""}
     ];
 
     var qwsa = new QuestionsSameAnswers({
