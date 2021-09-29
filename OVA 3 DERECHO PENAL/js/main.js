43f665d97a7a0a76ea08f3f71684162a49c31bc9
Sebastian Aguirre <sebasavmt@gmail.com>
2021-09-29 12:37:51 -0500

    Derecho penal ova 3 unidades y barra de progreso finalizadas

--- OVA 3 DERECHO PENAL/js/main.js
+++ OVA 3 DERECHO PENAL/js/main.js
@@ -83,7 +83,7 @@ function actualizarprogress(num){
     /*alert(naveg);
     alert(done);
     alert(active);*/
-    total_porcentaje = parseInt(((num)*100)/38);
+    total_porcentaje = parseInt(((num)*100)/44);
     $(".number").html(total_porcentaje+'%');
     var pixel = parseInt(((total_porcentaje*157)/100)+200);
     /*alert(pixel);
