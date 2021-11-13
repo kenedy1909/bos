f10c45dfca9631ba6901b18df2a297e164acbad1
JMAcevedo128k <juanmanuelacevedo238@gmail.com>
2021-11-13 13:49:16 -0500

    actu

--- OVA 1 Seminario de Sociedad y Desarrollo/js/views/unidad1.js
+++ OVA 1 Seminario de Sociedad y Desarrollo/js/views/unidad1.js
@@ -109,6 +109,7 @@ function controlSlides(num){
             scroll();
             break;
         case 1:
+
             setMigaja("Unidades de aprendizaje","1. La importancia de la formación humanística","");
             dragging = "";
             diff = "";
@@ -120,6 +121,7 @@ function controlSlides(num){
             scroll();
             break;
         case 2:
+            paraIframe(0);
             setMigaja("Unidades de aprendizaje","1. Introducción al curso","La importancia de la formación humanística");            
             break;
         case 3:
@@ -463,3 +465,32 @@ jQuery(document).ready(function ($) {
     });
 
 });
+
+function paraIframe(num){
+    switch (parseInt(num)) {
+        case 0:
+            $('.vid').attr('src', $('.vid').attr('src'));
+            break;
+        case 1:
+            $('.vid1').attr('src', $('.vid1').attr('src'));
+            break;
+        case 2:
+            $('.vid2').attr('src', $('.vid2').attr('src'));
+            break;
+        case 3:
+            $('.vid3').attr('src', $('.vid3').attr('src'));
+            break;
+        case 4:
+            $('.vid4').attr('src', $('.vid4').attr('src'));
+            break;
+        case 5:
+            $('.vid5').attr('src', $('.vid5').attr('src'));
+            break;
+        case 6:
+            $('.vid6').attr('src', $('.vid6').attr('src'));
+            break;
+        default:
+            break;
+    }
+    
+}
