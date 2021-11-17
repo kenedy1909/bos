b27b5dcf7f3786ca53b516a10726666f5775c098
JMAcevedo128k <juanmanuelacevedo238@gmail.com>
2021-11-17 12:31:25 -0500

    actu

--- OVA 1 fisica y laboratorio/js/views/unidad1.js
+++ OVA 1 fisica y laboratorio/js/views/unidad1.js
@@ -85,12 +85,14 @@ function controlSlides(num){
             setMigaja("Unidades de aprendizaje","1. Conceptos fundamentales","Magnitudes físicas y análisis dimensional");
             break;
         case 2:
+            paraIframe(0);
             setMigaja("Unidades de aprendizaje","1. Conceptos fundamentales","Magnitudes físicas y análisis dimensional");   
             break;
         case 3:
             setMigaja("Unidades de aprendizaje","1. Conceptos fundamentales","Magnitudes físicas y análisis dimensional");
             break;
         case 4:
+            paraIframe(0);
             setMigaja("Unidades de aprendizaje","1. Conceptos fundamentales","Magnitudes físicas y análisis dimensional");
             break;
         case 5:
@@ -1406,4 +1408,33 @@ function activi3(num){
             
             break;
     }
+}
+
+function paraIframe(num){
+    switch (parseInt(num)) {
+        case 0:
+            $('.vid').attr('src', $('.vid').attr('src'));
+            break;
+        case 1:
+            $('.vid1').attr('src', $('.vid1').attr('src'));
+            break;
+        case 2:
+            $('.vid2').attr('src', $('.vid2').attr('src'));
+            break;
+        case 3:
+            $('.vid3').attr('src', $('.vid3').attr('src'));
+            break;
+        case 4:
+            $('.vid4').attr('src', $('.vid4').attr('src'));
+            break;
+        case 5:
+            $('.vid5').attr('src', $('.vid5').attr('src'));
+            break;
+        case 6:
+            $('.vid6').attr('src', $('.vid6').attr('src'));
+            break;
+        default:
+            break;
+    }
+    
 }
