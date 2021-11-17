ffc18f9985996d6e8153817127e06a3067b46cad
JMAcevedo128k <juanmanuelacevedo238@gmail.com>
2021-11-17 14:53:54 -0500

    actu

--- OVA 2 inteligencia de negocios/js/views/unidad1.js
+++ OVA 2 inteligencia de negocios/js/views/unidad1.js
@@ -453,3 +453,32 @@ function funcionalidad_12(num) {
         $(".fun_12_btn_8").addClass('bg-blanco');
     }
 }
+
+function paraIframe(num){
+    switch (parseInt(num)) {
+        case 0:
+            $('.vid').attr('src', $('.vid').attr('src'));
+            break;
+        case 1:
+            $('.vid1').attr('src', $('.vid1').attr('src'));
+            break;
+        case 2:
+            $('.vid2').attr('src', $('.vid2').attr('src'));
+            break;
+        case 3:
+            $('.vid3').attr('src', $('.vid3').attr('src'));
+            break;
+        case 4:
+            $('.vid4').attr('src', $('.vid4').attr('src'));
+            break;
+        case 5:
+            $('.vid5').attr('src', $('.vid5').attr('src'));
+            break;
+        case 6:
+            $('.vid6').attr('src', $('.vid6').attr('src'));
+            break;
+        default:
+            break;
+    }
+    
+}
