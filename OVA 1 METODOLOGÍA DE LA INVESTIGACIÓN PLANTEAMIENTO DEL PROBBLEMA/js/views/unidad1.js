50652beaae93a554d4bc26d517edb3ba5acb357a
JMAcevedo128k <juanmanuelacevedo238@gmail.com>
2021-11-19 14:55:40 -0500

    actu

--- OVA 1 METODOLOGÍA DE LA INVESTIGACIÓN PLANTEAMIENTO DEL PROBBLEMA/js/views/unidad1.js
+++ OVA 1 METODOLOGÍA DE LA INVESTIGACIÓN PLANTEAMIENTO DEL PROBBLEMA/js/views/unidad1.js
@@ -93,12 +93,14 @@ function controlSlides(num){
             setMigaja("Unidades de aprendizaje","1. La ciencia: concepto y definición ", "Método hipotético");            
             break;
         case 3:
+            paraIframe(0);
             setMigaja("Unidades de aprendizaje","1. La ciencia: concepto y definición ", "Tipos de conocimiento");
             break;
         case 4:
             setMigaja("Unidades de aprendizaje","1. La ciencia: concepto y definición ", "Tipos de conocimiento");
             break;
         case 5:
+            paraIframe(0);
             setMigaja("Unidades de aprendizaje","1. La ciencia: concepto y definición ", "Actividad");
             dragging = "";
             diff = "";
@@ -849,4 +851,34 @@ function pant13(num){
         default:
             break;
     }
+}
+
+
+function paraIframe(num){
+    switch (parseInt(num)) {
+        case 0:
+            $('.vid').attr('src', $('.vid').attr('src'));
+            break;
+        case 1:
+            $('.vid1').attr('src', $('.vid1').attr('src'));
+            break;
+        case 2:
+            $('.vid2').attr('src', $('.vid2').attr('src'));
+            break;
+        case 3:
+            $('.vid3').attr('src', $('.vid3').attr('src'));
+            break;
+        case 4:
+            $('.vid4').attr('src', $('.vid4').attr('src'));
+            break;
+        case 5:
+            $('.vid5').attr('src', $('.vid5').attr('src'));
+            break;
+        case 6:
+            $('.vid6').attr('src', $('.vid6').attr('src'));
+            break;
+        default:
+            break;
+    }
+    
 }
