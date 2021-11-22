8fd43ceb411774e8bc813ad4a328f99dc3502932
Mariana Orrego <83648336+MarianaOrrego@users.noreply.github.com>
2021-11-22 11:55:08 -0500

    Modification about the hidden personage

--- OVA 4 Geometria Euclidiana/js/views/inicio.js
+++ OVA 4 Geometria Euclidiana/js/views/inicio.js
@@ -1,8 +1,7 @@
-$( document ).ready(function() {
-    setMigaja("Inicio","","");
-    $( ".ov-personaje").hide();
-    $( ".c-sidebar-nav > li").show();
-    $( ".c-sidebar-nav > .ov-inicio").show();
+$(document).ready(function() {
+    setMigaja("Inicio", "", "");
+    $(".c-sidebar-nav > li").show();
+    $(".c-sidebar-nav > .ov-inicio").show();
     var knob = "";
     var bar = "";
     var container = "";
@@ -14,7 +13,7 @@ $( document ).ready(function() {
     $(".modal_scroll4").on('click', modal_scroll4);
 });
 
-function modal_scroll3(){
+function modal_scroll3() {
     dragging = "";
     diff = "";
     newTop = "";
@@ -24,7 +23,8 @@ function modal_scroll3(){
     container = document.querySelector('.custom-scrollbar__inner3');
     scroll();
 }
-function modal_scroll4(){
+
+function modal_scroll4() {
     dragging = "";
     diff = "";
     newTop = "";
@@ -35,52 +35,55 @@ function modal_scroll4(){
     scroll();
 }
 
-function scroll(){
+function scroll() {
     // When the container is scrolled
     container.addEventListener('scroll', () => {
-      // If we are dragging the knob, do nothing
-      if (dragging) return;
+        // If we are dragging the knob, do nothing
+        if (dragging) return;
 
-      // Otherwise, set the knob position based on the scroll position
-      knob.style.top = container.scrollTop / (container.scrollHeight - container.offsetHeight) * 100 + '%';
+        // Otherwise, set the knob position based on the scroll position
+        knob.style.top = container.scrollTop / (container.scrollHeight - container.offsetHeight) * 100 + '%';
     });
 
     dragging = false;
 
     knob.addEventListener('mousedown', event => {
-        
-    console.log(knob+" "+bar+" "+container);
-      dragging = {
-        x: event.clientX,
-        y: event.clientY };
+
+        console.log(knob + " " + bar + " " + container);
+        dragging = {
+            x: event.clientX,
+            y: event.clientY
+        };
 
     });
     window.addEventListener('mousemove', event => {
-      if (dragging) {
-        // When dragging
-        event.preventDefault();
-        diff = {
-          x: event.clientX - dragging.x,
-          y: event.clientY - dragging.y };
+        if (dragging) {
+            // When dragging
+            event.preventDefault();
+            diff = {
+                x: event.clientX - dragging.x,
+                y: event.clientY - dragging.y
+            };
 
 
-        // Clamp the position of the knob to be a maximum of 
-        // the knobs container, and a minimum of 0
-        newTop = Math.max(0, Math.min(knob.offsetTop + diff.y, bar.offsetHeight));
-        knob.style.top = newTop + 'px';
+            // Clamp the position of the knob to be a maximum of 
+            // the knobs container, and a minimum of 0
+            newTop = Math.max(0, Math.min(knob.offsetTop + diff.y, bar.offsetHeight));
+            knob.style.top = newTop + 'px';
 
-        // Base the scroll offset on the knobs position
-        // in relation to the knobs container
-        scrollOffset = newTop / bar.offsetHeight * (container.scrollHeight - container.offsetHeight);
-        container.scrollTop = scrollOffset;
+            // Base the scroll offset on the knobs position
+            // in relation to the knobs container
+            scrollOffset = newTop / bar.offsetHeight * (container.scrollHeight - container.offsetHeight);
+            container.scrollTop = scrollOffset;
 
-        dragging = {
-          x: event.clientX,
-          y: event.clientY };
+            dragging = {
+                x: event.clientX,
+                y: event.clientY
+            };
 
-      }
+        }
     });
     window.addEventListener('mouseup', () => {
-      dragging = false;
+        dragging = false;
     });
 }
