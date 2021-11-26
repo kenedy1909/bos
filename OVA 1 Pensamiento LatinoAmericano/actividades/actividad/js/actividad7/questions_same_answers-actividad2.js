395d65d830ed1bbfba96124a83aa43220301c9c3
JMAcevedo128k <juanmanuelacevedo238@gmail.com>
2021-11-26 13:59:17 -0500

    actu

--- OVA 1 Pensamiento LatinoAmericano/actividades/actividad/js/actividad7/questions_same_answers-actividad2.js
+++ OVA 1 Pensamiento LatinoAmericano/actividades/actividad/js/actividad7/questions_same_answers-actividad2.js
@@ -173,16 +173,37 @@ QuestionsSameAnswers.prototype.score =  function () {
     var modalID = "#calificacionModal";
     var exito = false;
     var mensaje = "Inténtalo nuevamente.";
-    if (puntaje == 100) {
+    /*if (puntaje == 100) {
         exito = true;
         mensaje = "¡Felicitaciones!"
     }
     registrarActividad(puntaje);
     mostrarCalificacion(modalID, puntaje + '%', mensaje, exito, function () {
         self.reset(form);
-    });
+    });*/
+    if (puntaje == 100) {
+        
+      $('.img_res').html('<img src="../img/img7/bien.png" style="max-width: 90%;">');
+      $('.puntaje').text(puntaje+"%");
+      $('.mensaje').text("¡Felicitaciones!");
+      $('.btns_modal').html('<button type="button" class="btn" data-dismiss="modal" style="font-size: 20px;color: #420F0F;font-weight: bold;margin-top: -15px;">cerrar</button>');
+    }else if (puntaje >= 75 && puntaje < 100) {
+        
+      $('.img_res').html('<img src="../img/img7/bien.png" style="max-width: 90%;">');
+      $('.puntaje').text(Math.round(puntaje)+"%");
+      $('.mensaje').text("¡Felicitaciones!");
+      $('.btns_modal').html('<button type="button" class="btn" data-dismiss="modal" style="font-size: 20px;color: #420F0F;font-weight: bold;margin-top: -15px;">cerrar</button>');
+    }else{
+      $('.img_res').html('<img src="../img/img7/mal.png" style="max-width: 90%;">');
+      $('.mensaje').text("Inténtalo nuevamente.");
+      $('.puntaje').text(Math.round(puntaje)+"%");
+      $('.btns_modal').html('<button style="font-size: 20px;color: #420F0F;font-weight: bold;margin-top: -15px;" id="add" class="btn calificacion-intentar" data-dismiss="modal" onclick="reload()">Volver a intentar</button>');
+    }
+    $("#exampleModal").modal("show");
 }
-
+function reload(){
+    location.reload();
+}
 QuestionsSameAnswers.prototype.prepare_files =  function () {
     /* Crea y carga los archivos necesarios para que el módulo funcione */
 
