62748187dbd66d5975c33ac2a79f4326d8e69996
Juan Esteban Londoño Gomez <72523178+Estebanxz11@users.noreply.github.com>
2022-01-28 23:15:53 -0500

    update

--- OVA 2 Simulación Gerencial - Generación de escenarios estratégicos/js/views/unidad1.js
+++ OVA 2 Simulación Gerencial - Generación de escenarios estratégicos/js/views/unidad1.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
     paso = false;
+    $('.modal').on('hidden.bs.modal', function (event) {
+        alert();
+        $('body').addClass('p-0');
+    });
     $('.menu1').removeClass('d-none');
     $('.menu1').addClass('c-show');
     $( ".zoomContainer" ).remove();
