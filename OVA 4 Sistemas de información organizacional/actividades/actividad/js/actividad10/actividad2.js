0e7ba9c7bea1cc5582c9d5d63da0dc8354dc3ff2
Sebastián Aguirre <sebasavmt@gmail.com>
2022-02-08 17:22:47 -0500

    sistemas ova 4

--- OVA 4 Sistemas de información organizacional/actividades/actividad/js/actividad10/actividad2.js
+++ OVA 4 Sistemas de información organizacional/actividades/actividad/js/actividad10/actividad2.js
@@ -3,15 +3,22 @@ $(document).ready(function() {
 				<img class="img_questionario" src="../../../assets/img/img_ova/ec2.png">
 				`;
 	var img2 = `
+				<img class="img_questionario" src="../../../assets/img/img_ova/ec1.png">
+				`;
+	var img3 = `
 				<img class="img_questionario" src="../../../assets/img/img_ova/ec2.png">
+				`;
+	var img4 = `
+				<img class="img_questionario" src="../../../assets/img/img_ova/ven3.png">
+				`;
+	var img5 = `
+				<img class="img_questionario" src="../../../assets/img/img_ova/redsocial1.png">
 				`;
 	$(".img_questionario1").html(img1);
 	$(".img_questionario2").html(img2);
-	$(".img_questionario3").html(img1);
-	$(".img_questionario4").html(img2);
-	$(".img_questionario5").html(img1);
-	$(".img_questionario6").html(img2);
-	$(".img_questionario7").html(img1);
+	$(".img_questionario3").html(img3);
+	$(".img_questionario4").html(img4);
+	$(".img_questionario5").html(img5);
 
 	$("#cuestionario-2").css('display','none');
 	$("#cuestionario-3").css('display','none');
