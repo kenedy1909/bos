e39df04fd3f4b64b74f61c17fab0ac59dc380f0e
Juan Pablo Suarez Ramirez <86664381+juanp-suarezr@users.noreply.github.com>
2022-02-08 11:19:52 -0500

    inv 3 ova 1 8/02/22

--- OVA 1 PROYECTO-DE-INVESTIGACIÓN-PARTE-I-(PROBLEMATIZACIÓN)/js/views/unidad1.js
+++ OVA 1 PROYECTO-DE-INVESTIGACIÓN-PARTE-I-(PROBLEMATIZACIÓN)/js/views/unidad1.js
@@ -9,7 +9,7 @@ $( document ).ready(function() {
     $(".menu3").removeClass('c-show');
     $(".menu4").removeClass('c-show');
     star_uni = 1;
-    // $('.js_uni').html('<script src="js/views/unidades.js"></script>');
+    
 
     $('#smartwizard').smartWizard({
         loader:"show",
@@ -201,7 +201,7 @@ function controlSlides(num){
             $(".menu1").addClass('d-none');
             document.getElementById("next").style.display="none";
             document.getElementById("prev").style.display="block";
-            
+            $('.js_uni').html('<script src="js/views/unidades.js"></script>');
             break;
 
         default:
